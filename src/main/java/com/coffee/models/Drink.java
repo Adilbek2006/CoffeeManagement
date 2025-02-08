2fba9f0f153585177a8736f140969a5f9a99190d
Zayniddin <wakhbakh@gmail.com>
2025-02-08 09:23:04 +0500

    methods for drinks and foods

--- src/main/java/com/coffee/models/Drink.java
+++ src/main/java/com/coffee/models/Drink.java
@@ -1,4 +1,38 @@
 package com.coffee.models;
 
-public class Drink {
+public class Drink implements Item {
+    private String name;
+    private double price;
+    private String size;
+
+    public Drink(String name, double price, String size) {
+        this.name = name;
+        this.price = price;
+        this.size = size;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getSize() {
+        return size;
+    }
+
+    @Override
+    public String getType() {
+        return "drink";
+    }
+
+    @Override
+    public String toString() {
+        return size + " " + name + " ($" + price + ")";
+    }
 }
