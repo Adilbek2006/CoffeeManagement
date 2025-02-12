6702ee19f6114871140dc0d2cc9199378d32f7c7
KNari06 <narimankajdar@gmail.com>
2025-02-13 01:59:57 +0500

    create interface constructors

--- src/main/java/com/coffee/database/DatabaseService.java
+++ src/main/java/com/coffee/database/DatabaseService.java
@@ -1,5 +1,9 @@
 package com.coffee.database;
 
-public interface DatabaseService {
+import java.util.List;
 
+public interface DatabaseService {
+    List<String> getMenu(String type);
+    void saveOrder(String name, double price, String size, String type);
+    List<String> getOrders();
 }
