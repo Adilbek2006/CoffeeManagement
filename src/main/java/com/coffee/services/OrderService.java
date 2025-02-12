55dc144d0f890130f9d8a919a0778fd2bbcaee2b
Zayniddin <wakhbakh@gmail.com>
2025-02-13 01:53:11 +0500

    adding class for order service and it's interface

---
+++ src/main/java/com/coffee/services/OrderService.java
@@ -0,0 +1,8 @@
+package com.coffee.services;
+
+import com.coffee.database.DatabaseService;
+import java.util.List;
+
+public class OrderService {
+    //Order service class
+}
---
+++ src/main/java/com/coffee/services/OrderServiceInterface.java
@@ -0,0 +1,5 @@
+package com.coffee.services;
+
+public interface OrderServiceInterface {
+    //Interface of the orders
+}
